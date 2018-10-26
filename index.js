afd62c278a499247c08f70437673a7ec68ada26d
Lionel Oto <lionel.oto@gmail.com>
2018-10-26 11:10:20 -0400

    Fixed authentication error with ServiceNow OAuth, refactored index.js, added authentication logic to server

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ const sessions = require('./lib/sessions');
 const echobot = require('./lib/echobot-adapter/bot');
 const azurebot = require('./lib/azurebot-adapter/bot');
 const rasabot = require('./lib/rasabot-adapter/bot');
+const incidents = require('./lib/incidents/incident');
 
 const AGENTS = { 'echobot': echobot, 'azurebot': azurebot, 'rasabot': rasabot };
 const default_agent = 'echobot';
 
+fastify.decorate('authenticate', async (token) => {
+    return await sessions.find(token);
+});
+
+fastify.decorateReply('invalidAuthentication', function () {
+    this.type('application/json');
+    this.code(401);
+    this.send({ error: 'Invalid conversation token' });
+});
+
 fastify.post('/conversation/start', async (_request, reply) => {
-    reply.type('application/json').code(201)
     let session = await sessions.new(default_agent);
-    return { token: session.token }
+    reply.type('application/json');
+    reply.code(201);
+    reply.send({ token: session.token });
 });
 
 const conversation_say_opts = {
@@ -32,20 +44,20 @@ const conversation_say_opts = {
 fastify.post('/conversation/say', conversation_say_opts, async (request, reply) => {
     let message = request.body.message;
     let token = request.body.token;
-    let session = await sessions.find(token);
-    if (session) {
-        reply.type('application/json').code(200);
-        return { reply: await AGENTS[session.agent_name].sendMessage(token, message) };
-    } else {
-        reply.type('application/json').code(404);
-        return { error: 'Unknown conversation token' };
-    }
+
+    let session = await fastify.authenticate(token);
+    if (!session) reply.invalidAuthentication();
+
+    reply.type('application/json')
+        .code(200)
+        .send({ reply: await AGENTS[session.agent_name].sendMessage(token, message) });
 });
 
-fastify.get('/agents/list', async (request, reply) => {
-    reply.type('application/json').code(200)
-    return { agents: Object.keys(AGENTS) }
-})
+fastify.get('/agents/list', async (_request, reply) => {
+    reply.type('application/json')
+        .code(200)
+        .send({ agents: Object.keys(AGENTS) });
+});
 
 const agent_select_opts = {
     schema: {
@@ -63,19 +75,34 @@ const agent_select_opts = {
 fastify.post('/agent/select', agent_select_opts, async (request, reply) => {
     let name = request.body.name;
     let token = request.body.token;
-    let session = await sessions.find(token);
-    if (session) {
-        if (Object.keys(AGENTS).includes(name)) {
-            await sessions.update(token, name);
-            reply.type('application/json').code(200)
-            return { agent: name }
-        } else {
-            reply.type('application/json').code(404);
-            return { error: 'Unknown agent' };
-        }
+
+    let session = await fastify.authenticate(token);
+    if (!session) reply.invalidAuthentication();
+
+    if (Object.keys(AGENTS).includes(name)) {
+        await sessions.update(token, name);
+        reply.type('application/json')
+            .code(200)
+            .send({ agent: name });
     } else {
-        reply.type('application/json').code(404);
-        return { error: 'Unknown conversation token' };
+        reply.type('application/json')
+            .code(404)
+            .send({ error: 'Unknown agent' });
+    }
+});
+
+fastify.get('/incidents/list', async (_request, reply) => {
+    let response;
+    try {
+        response = await incidents.all();
+        reply.type('application/json')
+            .code(200)
+            .send({ incidents: response });
+    } catch (error) {
+        let errorCode = error.code ? error.code : 500;
+        reply.type('application/json')
+            .code(errorCode)
+            .send({ error: error.message });
     }
 });
 
